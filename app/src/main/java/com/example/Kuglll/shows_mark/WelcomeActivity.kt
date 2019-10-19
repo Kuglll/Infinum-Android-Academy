d909796c2aedb9199b3bf07158974c667b801a4a
Kuglll <mark.frelih@gmail.com>
2019-10-19 16:05:49 +0200

    added transitions between activities

--- app/src/main/java/com/example/Kuglll/shows_mark/WelcomeActivity.kt
+++ app/src/main/java/com/example/Kuglll/shows_mark/WelcomeActivity.kt
@@ -1,8 +1,15 @@
 package com.example.Kuglll.shows_mark
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_welcome.*
+import androidx.core.os.HandlerCompat.postDelayed
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.os.Handler
+
 
 class WelcomeActivity : AppCompatActivity() {
 
@@ -12,5 +19,13 @@ class WelcomeActivity : AppCompatActivity() {
 
         var username : String? = intent.getStringExtra("username").split("@")[0]
         welcomeUserTextview.text = "Welcome $username"
+
+        Handler().postDelayed(this::startShowActivity, 2000)
     }
+
+    fun startShowActivity(){
+        val intent = Intent(this, ShowActivity::class.java)
+        startActivity(intent)
+    }
+
 }
